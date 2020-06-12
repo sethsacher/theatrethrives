9846e7b80cbeab77ada260e9000dc7c8f30dbf85
Seth Sacher <ssacher@collabralink.com>
2020-06-11 22:42:01 -0400

    Carousel updates

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -7,16 +7,22 @@
 
 import React from "react"
 
-import { Carousel, View, Mask } from "react-bootstrap"
+import { Carousel, Container } from "react-bootstrap"
 
 import image1 from "../images/RCP_GGLAM.jpg"
 import image2 from "../images/RCP_Aida.jpg"
 import image3 from "../images/RCP_TMM.jpg"
 
 const CarouselComp = () => (
-  <Carousel>
+  <Carousel controls={false} fade={false} indicators={false}>
     <Carousel.Item>
       <img className="d-block w-100" src={image1} alt="First slide" />
+    </Carousel.Item>
+    <Carousel.Item>
+      <img className="d-block w-100" src={image2} alt="First slide" />
+    </Carousel.Item>
+    <Carousel.Item>
+      <img className="d-block w-100" src={image3} alt="First slide" />
     </Carousel.Item>
   </Carousel>
 )
