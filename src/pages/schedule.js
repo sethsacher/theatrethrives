20a037a1c0f58966aecc6d2d65d34cc22493c62d
Seth Sacher <ssacher@collabralink.com>
2020-06-12 17:30:44 -0400

    Schedule structure

--- src/pages/schedule.js
+++ src/pages/schedule.js
@@ -1,16 +1,41 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql, useStaticQuery } from "gatsby"
 
+import { Container, Row } from "react-bootstrap"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import ScheduleItem from "../components/scheduleItem"
 
-const SchedulePage = () => (
-  <Layout pageInfo={{ pageName: "schedule" }}>
-    <SEO title="Schedule" />
-    <h1>SCHEDULE</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-)
+const SchedulePage = () => {
+  const query = useStaticQuery(graphql`
+    query {
+      events: allScheduleCsv {
+        nodes {
+          Description
+          Event
+          Image
+          Time
+        }
+      }
+    }
+  `)
+  const events = query.events.nodes.map(event => {
+    return <ScheduleItem event={event} />
+  })
+  return (
+    <Layout pageInfo={{ pageName: "schedule" }}>
+      <SEO title="Schedule" />
+      <Container>
+        <Row>
+          <h2>Event Schedule</h2>
+        </Row>
+        <Row>
+          <p>Here is our event schedule</p>
+        </Row>
+        {events}
+      </Container>
+    </Layout>
+  )
+}
 
 export default SchedulePage
