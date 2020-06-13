1676227d24571de10ea4a4f551a77eb67d8d896c
Seth Sacher <ssacher@collabralink.com>
2020-06-12 20:36:28 -0400

    Setup environment variables

--- src/pages/donate.js
+++ src/pages/donate.js
@@ -1,16 +1,17 @@
 import React from "react"
 import { Link } from "gatsby"
 
+import DropIn from "braintree-web-drop-in-react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const DonatePage = () => (
-  <Layout pageInfo={{ pageName: "donate" }}>
-    <SEO title="Donate" />
-    <h1>DONATE</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-)
+const DonatePage = () => {
+  return (
+    <Layout pageInfo={{ pageName: "donate" }}>
+      <SEO title="Donate" />
+      <h1>DONATE</h1>
+    </Layout>
+  )
+}
 
 export default DonatePage
