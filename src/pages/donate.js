baa1fef6168ae081df23ff79d707ba62c0c054a4
Seth Sacher <ssacher@collabralink.com>
2020-06-13 11:33:52 -0400

    Remove Braintree, moving to Stripe

--- src/pages/donate.js
+++ src/pages/donate.js
@@ -1,8 +1,7 @@
 import React from "react"
-import { Link } from "gatsby"
+// import { Link } from "gatsby"
 import { Form, Button } from "react-bootstrap"
 
-import DropIn from "braintree-web-drop-in-react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
@@ -15,98 +14,40 @@ class DonatePage extends React.Component {
     nonce: null,
   }
 
-  async componentDidMount() {
-    // Get a client token for authorization from your server
-    const response = await fetch(process.env.BT_URL, {
-      method: "POST",
-      headers: {
-        Authorization: `Basic ${process.env.BT_BASE64}`,
-        "Braintree-Version": "2020-06-12",
-        "Content-Type": "application/json",
-      },
-      body: '{"query": "mutation {createClientToken {clientToken}}"}',
-    })
-    const clientResponse = await response.json()
-    if (clientResponse.data) {
-      this.setState({
-        clientToken: clientResponse.data.createClientToken.clientToken,
-      })
-    }
-  }
+  // async componentDidMount() {
 
-  handleSubmit = async event => {
-    event.preventDefault()
+  // }
 
-    if (!this.state.nonce) {
-      // Send submitted payment method; get nonce
-      const responsePaymentMethod = await this.instance.requestPaymentMethod()
-      console.log(JSON.stringify(responsePaymentMethod))
-      this.setState({ nonce: responsePaymentMethod.nonce })
-    } else {
-      // Use nonce to charge the payment method
-      const response = await fetch(process.env.BT_URL, {
-        method: "POST",
-        headers: {
-          Authorization: `Basic ${process.env.BT_BASE64}`,
-          "Braintree-Version": "2020-06-12",
-          "Content-Type": "application/json",
-        },
-        body: `{
-        "query": "mutation ExampleCharge($input: ChargePaymentMethodInput!) {
-          chargePaymentMethod(input: $input) {
-            transaction {
-              id
-              status
-            }
-          }
-        }",
-        "variables": {
-          "input": {
-            "paymentMethodId": "${this.state.nonce}",
-            "transaction": {
-              "amount": "11.23"
-            }
-          }
-        }
-      }`,
-      })
-      console.log(JSON.stringify(response))
-      this.setState({ submit: true })
-    }
-  }
+  // handleSubmit = async event => {
+  //   event.preventDefault()
+  //     this.state({
+  //       submit: true
+  //     })
+  //   }
+  // }
 
-  DonationForm = () => {
-    // Confirmation page
-    if (this.state.submit && this.state.submit === true) {
-      return <h4>Submitted!</h4>
-    }
-    // Wait for client token to be fetched
-    if (!this.state.clientToken) {
-      return <h4>Loading...</h4>
-    }
-    // Main payment form
-    return (
-      <Form id="donation-form" onSubmit={this.handleSubmit}>
-        <Form.Group controlId="donationAmount">Amount</Form.Group>
-        <Form.Group controlId="braintreeDropIn">
-          <DropIn
-            options={{ authorization: this.state.clientToken }}
-            onInstance={instance => (this.instance = instance)}
-          />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          {this.state.nonce ? "Donate" : "Check Payment"}
-        </Button>
-      </Form>
-    )
-  }
+  // DonationForm = () => {
+  //   // Confirmation page
+  //   if (this.state.submit && this.state.submit === true) {
+  //     return <h4>Submitted!</h4>
+  //   }
+  //   // Main payment form
+  //   return (
+  //     <Form id="donation-form" onSubmit={this.handleSubmit}>
+  //       <Form.Group controlId="donationAmount">Amount</Form.Group>
+  //       <Button variant="primary" type="submit">
+  //         {this.state.nonce ? "Donate" : "Check Payment"}
+  //       </Button>
+  //     </Form>
+  //   )
+  // }
 
   render() {
     return (
       <Layout pageInfo={{ pageName: "donate" }}>
         <SEO title="Donate" />
         <h1>DONATE</h1>
-        <this.DonationForm />
+        {/* <this.DonationForm /> */}
       </Layout>
     )
   }
