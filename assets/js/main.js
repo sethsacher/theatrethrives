eae214a1d9da60469747fd5e6c29921b451b138f
Seth Sacher <ssacher@collabralink.com>
2020-06-15 08:46:28 -0400

    Countdown timer

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,6 +7,21 @@
 !(function ($) {
   'use strict';
 
+  // Countdown Timer
+  $('#clock')
+    .countdown('2020/07/17')
+    .on('update.countdown', function (event) {
+      var $this = $(this).html(
+        event.strftime(
+          '' +
+            '<span class="h1 font-weight-bold">%D</span> Day%!d' +
+            '<span class="h1 font-weight-bold">%H</span> Hr' +
+            '<span class="h1 font-weight-bold">%M</span> Min' +
+            '<span class="h1 font-weight-bold">%S</span> Sec'
+        )
+      );
+    });
+
   // Back to top button
   $(window).scroll(function () {
     if ($(this).scrollTop() > 100) {
